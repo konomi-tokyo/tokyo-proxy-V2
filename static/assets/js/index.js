1d3be98a7a1976f863575ad43053cdbb3ddc74d7
CyberTitus <82715493+CyberTitus@users.noreply.github.com>
2023-04-19 19:54:08 +0100

    Initial Commit

---
+++ static/assets/js/index.js
@@ -0,0 +1,41 @@
+const input = document.querySelector("input");
+input.onkeypress = function(e){
+    if (!e) e = window.event;
+    var keyCode = e.code || e.key;
+    if (keyCode == 'Enter'){
+      go(input.value)
+      return false;
+    }
+  }
+
+function blank3(){
+    let inFrame
+    
+    try {
+        inFrame = window !== top
+    } catch (e) {
+        inFrame = true
+    }
+    
+    if (!inFrame && !navigator.userAgent.includes("Firefox")) {
+        const popup = open("about:blank", "_blank")
+        if (!popup || popup.closed) {
+            alert("Enable Popups to hide from history...")
+        } else {
+            const doc = popup.document
+            const iframe = doc.createElement("iframe")
+            const style = iframe.style
+            const link = doc.createElement("link")
+    
+            doc.title = "loading..."
+            link.rel = "icon";
+            link.href = "";
+            iframe.src = location.href
+            style.position = "fixed"
+            style.top = style.bottom = style.left = style.right = 0
+            style.border = style.outline = "none"
+            style.width = style.height = "100%"
+            doc.body.appendChild(iframe)
+            //location.replace("https://google.com")
+        }
+    }}
