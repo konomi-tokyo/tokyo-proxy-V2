049371b63538f5212587a9fc86568a15fdf695fb
�東�京� <131155313+konomi-tokyo@users.noreply.github.com>
2023-11-19 18:41:58 +0900

    Update sw.js

--- static/sw.js
+++ static/sw.js
@@ -1,7 +1,7 @@
 importScripts("./uv/uv.bundle.js");
 importScripts("./uv/uv.config.js");
 importScripts("./uv/uv.sw.js");
-
+ 
 const sw = new UVServiceWorker();
 let userKey = new URL(location).searchParams.get('userkey');
 
